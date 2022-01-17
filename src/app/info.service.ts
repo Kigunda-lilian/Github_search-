eb449b166d9206df9071eb3613a1e2c92527722f
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-01-17 16:31:01 +0300

    created a interface and assigned it property with expexted data type value

--- src/app/info.service.ts
+++ src/app/info.service.ts
@@ -2,12 +2,30 @@ import { Injectable } from '@angular/core';
 import{HttpClient} from'@angular/common/http';
 import { RepoClass } from './repo-class';
 import { UserClass } from './user-class';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoService {
+  users!:UserClass
+
 
   constructor() { }
+
+
+
+  getUser(){
+    interface Apiresponce{
+      login:string,
+        name:string,
+        avatar_url:string,
+        bio:string,
+       _repos:string,
+        followers:string,
+        following:string,
+         created_at:Date,
+    }
+  }
 }
