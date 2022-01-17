f1d565ef763cdbc4c4e85ec2f4495aaf175a5b6b
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-01-17 16:25:44 +0300

    added class to inject the srvice

--- src/app/info.service.spec.ts
+++ src/app/info.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HttpService } from './http.service';
+import { InfoService } from './info.service';
 
-describe('HttpService', () => {
-  let service: HttpService;
+describe('InfoService', () => {
+  let service: InfoService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpService);
+    service = TestBed.inject(InfoService);
   });
 
   it('should be created', () => {
---
+++ src/app/info.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import{HttpClient} from'@angular/common/http';
+import { RepoClass } from './repo-class';
+import { UserClass } from './user-class';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoService {
+
+  constructor() { }
+}
